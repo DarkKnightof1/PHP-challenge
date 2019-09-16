b6c5fa5ba374737fc222f87286857d76eee35371
DarkKnightof1 <mischa@boskat.eu>
2019-09-16 10:16:50 +0200

    opdracht 2

--- Index.php
+++ Index.php
@@ -3,7 +3,29 @@ $a = "Ik heb niet gefaald";
 $b = "die niet werken";
 $c = 10000;
 $d= $a . ". Ik heb alleen " . $c . " manieren gevonden " . $b . " .";
+
+$e = "die geen fout";
+$f = "en";
+$g = "niet.";
+$h= "Mens" . $f. " " . $e . $f . ", werk" . $f . " " . $g;
+
+$i = "ry";
+$j = ", but";
+$k = "fail";
+$l= "T" . $i . " and " . $k . $j . " never " . $k . " to " . "t" . $i . ".";
+
+$m = "\"omgaan";
+$n = "met";
+$o = "teleu";
+$p = "De cursus " . $m . " " . $n . " " . $o . "rstellingen\" kan vanavond helaas niet doorgaan.";
+
 echo $d;
+echo "<br>";
+echo $h;
+echo "<br>";
+echo $l;
+echo "<br>";
+echo $p;
 /**
  * Created by PhpStorm.
  * User: misch
