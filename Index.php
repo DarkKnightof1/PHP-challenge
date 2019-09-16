1183fd3a36082867519e597b43db16b92cdcb150
DarkKnightof1 <mischa@boskat.eu>
2019-09-16 09:58:36 +0200

    opdracht 2

---
+++ Index.php
@@ -0,0 +1,12 @@
+<?php
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+$d= $a . ". Ik heb alleen " . $c . " manieren gevonden " . $b . " .";
+echo $d;
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 16-9-2019
+ * Time: 09:53
+ */
