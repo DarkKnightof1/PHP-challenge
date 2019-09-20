7f9f7b652651e5a1506c934a774dc3b43cb5f2c4
DarkKnightof1 <mischa@boskat.eu>
2019-09-20 10:02:15 +0200

    opdracht3.2

---
+++ opdracht3-2.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 20-9-2019
+ * Time: 09:55
+ */
+
+	$x = 35;
+while($x >= 25) {
+    echo "hoppelepee";
+    $x--;
+}
+echo "<br>";
+$b = 25;
+for ($a = 35; $a >= $b; $a --) {
+    echo " lol ";
+}
+
+
